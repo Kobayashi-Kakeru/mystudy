c4cdc4aa029a22b7773b0af9aa23e267a30cb783
小林翔 <kobayashikakeru@kobayashishounoMacBook-Air.local>
2021-04-23 12:49:52 +0900

    myextendngstudy

---
+++ connect_db.php
@@ -0,0 +1,35 @@
+<?php
+$json = file_get_contents('php://input');
+$string = json_decode($json);
+$num = $string->num;
+$uuid = $string->uuid;
+$data;
+
+try{
+$url = parse_url(getenv('DATABASE_URL'));
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+
+$sql = 'select * from list_db where uuid = :uuid';
+$db_data = $pdo->prepare($sql);
+$db_data->execute(array(':uuid'=>$uuid));
+$result = $db_data->fetch();
+if($num <= 5){
+$data = $result['url1'];
+}else if(5 < $num && $num <= 10){
+$data = $result['url2'];
+}else if(10 < $num){
+$data = $result['url3'];
+}
+
+echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    error_log('error');
+    die();
+}
+
+$pdo = null;
+
+?>
